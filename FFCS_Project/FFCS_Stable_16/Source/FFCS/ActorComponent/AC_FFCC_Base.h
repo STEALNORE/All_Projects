7f2882f8f55b117f2f4087adb50db41142273731
STEALNORE <artemanikin2118@gmail.com>
2025-10-10 09:37:51 +0400

    Counter System

---
+++ FFCS_Project/FFCS_Stable_16/Source/FFCS/ActorComponent/AC_FFCC_Base.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "FFCS/Asset/FFCS_Data.h"
+#include "AC_FFCC_Base.generated.h"
+
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class FFCS_API UAC_FFCC_Base : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAC_FFCC_Base();
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	EAttackHitDirection HitDirection(AActor* Instigator);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void PlayHitMontage(EAttackHitDirection HitDirection);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetCurrentEnemy(AActor* EnemyRef);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure, BlueprintCallable)
+	AActor* GetCurrentEnemy();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetTag(const FGameplayTag& InTag);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure, BlueprintCallable)
+	FGameplayTag GetTag();
+
+//protected:
+//	// Called when the game starts
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
