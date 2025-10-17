feaf2757f0dd976275e18b37453cf930e1f7522a
STEALNORE <artemanikin2118@gmail.com>
2025-10-17 12:17:47 +0400

    Finishers

---
+++ FFCS_Project/FFCS_21/Source/FFCS/AnimNotifyStates/ANS_PlayerTeleport.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANS_PlayerTeleport.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FFCS_API UANS_PlayerTeleport : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+
+public:
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, const FAnimNotifyEventReference& EventReference) override;
+
+public:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	float Offset = 50.0f;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	float InterpSpeed = 5.0f;
+};
