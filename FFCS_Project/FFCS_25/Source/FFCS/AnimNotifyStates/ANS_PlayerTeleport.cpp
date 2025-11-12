953f22cfbf547b86f119b145bbfa883d11909205
STEALNORE <artemanikin2118@gmail.com>
2025-11-12 11:06:08 +0400

    Major Counter Bug Fix

---
+++ FFCS_Project/FFCS_25/Source/FFCS/AnimNotifyStates/ANS_PlayerTeleport.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ANS_PlayerTeleport.h"
+#include "FFCS/ActorComponent/AC_FFCC_Base.h"
+#include "FFCS/Character/FFCC_CharacterBase.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UANS_PlayerTeleport::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+	
+	if (MeshComp->GetOwner())
+	{
+		UAC_FFCC_Base* FFCC = Cast<UAC_FFCC_Base>(MeshComp->GetOwner()->GetComponentByClass(UAC_FFCC_Base::StaticClass()));
+		if(FFCC)
+		{
+			if(FFCC->GetCounterEnemy())
+			{
+				FVector StartLoc = MeshComp->GetOwner()->GetActorLocation();
+				FVector EndLoc = FFCC->GetCounterEnemy()->GetActorLocation();
+				FVector Direction = (StartLoc - EndLoc).GetSafeNormal();
+				FVector TeleportLoc = EndLoc + (Direction * Offset);
+				FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(StartLoc, EndLoc);
+				FVector InterpLoc = FMath::VInterpTo(StartLoc, TeleportLoc, FrameDeltaTime, InterpSpeed);
+				FFCC->GetCounterEnemy()->SetActorRotation(UKismetMathLibrary::FindLookAtRotation(EndLoc, StartLoc));
+				MeshComp->GetOwner()->SetActorLocationAndRotation(InterpLoc, LookAtRotation);
+			}
+			
+		}
+	}
+}
+
+void UANS_PlayerTeleport::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if(MeshComp->GetOwner())
+	{
+		AFFCC_CharacterBase* Char = Cast<AFFCC_CharacterBase>(MeshComp->GetOwner());
+		if(Char)
+		{
+			Char->DashCounterMontage();
+		}
+	}
+}
