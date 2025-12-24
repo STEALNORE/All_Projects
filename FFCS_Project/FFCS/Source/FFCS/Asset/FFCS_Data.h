7b41c42cfdd3647888b8af2082ac7be47b79136b
STEALNORE <artemanikin2118@gmail.com>
2025-12-24 22:23:45 +0400

    FFCS

---
+++ FFCS_Project/FFCS/Source/FFCS/Asset/FFCS_Data.h
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "FFCS_Data.generated.h"
+
+
+UENUM(BlueprintType)
+enum EAttackHitDirection : uint8
+{
+	Fwd UMETA(DisplayName = "Forward"),
+	Bwd UMETA(DisplayName = "Backward"),
+	Lt UMETA(DisplayName = "Left"),
+	Rt UMETA(DisplayName = "Right")
+};
+
+UENUM(BlueprintType)
+enum EAIStates : uint8
+{
+	GetInRange,
+	GetBack,
+	Wait,
+	LookForWeapons,
+	Strafe,
+	Attack,
+	Stun,
+	ExitStun
+};
+
+USTRUCT(BlueprintType)
+struct FPlayerAttackAnimData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* AttackMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PlayRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Offset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Damage;
+
+	FPlayerAttackAnimData()
+	{
+		PlayRate = 1.0f;
+		Offset = 100.0f;
+		Damage = 10.0f;
+	}
+	
+};
+
+USTRUCT(BlueprintType)
+struct FFinisherAnimData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* FinisherMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* HitMontage;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PlayRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Offset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Damage;
+
+	FFinisherAnimData()
+	{
+		PlayRate = 1.0f;
+		Offset = 100.0f;
+		Damage = 10.0f;
+	}
+	
+};
+
+/*
+UCLASS()
+class FFCS_API UFFCS_Data : public UObject
+{
+	GENERATED_BODY()
+	
+}; */
---
+++ FFCS_Project/FFCS/Source/FFCS/Interfaces/FFCS_Interface_Character.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FFCS_Interface_Character.h"
+
+// Add default functionality here for any IFFCS_Interface_Character functions that are not pure virtual.
